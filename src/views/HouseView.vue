4897ea0415cf1c1fd62be0e6ae22085524693a6f
ignatJasevits <igjase@ttu.ee>
2023-11-04 12:42:20 +0200

    Company page has option(button) "Houses" which shows company houses addresses. Upon click on each of those addresses, it is visible which services does that house have

--- src/views/HouseView.vue
+++ src/views/HouseView.vue
@@ -3,37 +3,60 @@
     <h1>Houses</h1>
     <table>
       <tr>
-        <th>House id</th>
         <th>Address</th>
       </tr>
-      <tr v-for="house in houses">
-        <td>{{house.id}}</td>
-        <td>{{house.address}}</td>
+      <tr v-for="house in houses" :key="house.id">
+        <td @click="loadServices(house.id)">{{ house.address }}</td>
       </tr>
     </table>
+    <div v-if="selectedHouse">
+      <h2>Services for {{ selectedHouse.address }}</h2>
+      <ul>
+        <li v-for="service in services" :key="service.id">{{ service.serviceName }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
-  import axios from "axios";
-  import {h, onMounted, ref} from "vue";
+import axios from "axios";
+import { ref, onMounted } from "vue";
 
-  const houses = ref([])
+const houses = ref([]);
+const selectedHouse = ref(null);
+const services = ref([]);
 
-  onMounted(() => {
-    axios.get("/api/house")
-        .then(response => {
-          houses.value = response.data
-        })
-    console.log("test");
-  })
+const loadServices = async (houseId) => {
+  selectedHouse.value = houses.value.find((house) => house.id === houseId);
+  services.value = await fetchServices(houseId);
+};
+
+const fetchServices = async (houseId) => {
+  try {
+    const response = await axios.get(`/api/service/byHouseId/${houseId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching services:", error);
+    return [];
+  }
+};
+
+onMounted(async () => {
+  try {
+    const response = await axios.get("/api/house");
+    houses.value = response.data;
+  } catch (error) {
+    console.error("Error fetching houses:", error);
+  }
+});
 </script>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .house {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
   }
 }
