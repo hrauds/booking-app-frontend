1d03a87b8b66910b25ee1cfaa71bdde782a70ce4
hrauds <hanna.raudsepp@gmail.com>
2023-11-19 17:51:18 +0200

    Added a button that leads to signup view, and updated style

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,60 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Company from './components/Company.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div id="app">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/company/houses">Houses</RouterLink>
+    </nav>
+    <div class="content">
 
-    <div class="wrapper">
-      <Company msg="Game Time Booking Service" />
+      <template v-if="$route.path === '/'">
+        <h1>Welcome to Game Time Booking Service</h1>
+        <router-link to="/signup">
+          <button>Book Now</button>
+        </router-link>
+      </template>
 
-      <nav>
-        <RouterLink to="/company/houses">Houses</RouterLink>
-      </nav>
+      <RouterView />
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#app {
+  text-align: center;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
+  position: fixed;
   text-align: center;
-  margin-top: 2rem;
-}
+  top: 4rem;
+  left: 4rem;
+  right: 4rem;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  padding: 2rem;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  margin-right: 4rem;
+  font-size: 1.5em;
 }
 
-nav a:first-of-type {
-  border: 0;
+.content {
+  margin-top: 12rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+h1 {
+  font-size: 2em;
+  white-space: nowrap;
 }
+
+button {
+  font-size: 1.2em;
+  padding: 0.5em 1em;
+  border: none;
+  background-color: palegreen;
+  cursor: pointer;
+  margin-top: 1rem;
+}
+
 </style>
