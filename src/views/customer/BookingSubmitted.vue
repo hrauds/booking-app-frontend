746dcbaf7aa82482f308eba687eca44a394561de
hrauds <hanna.raudsepp@gmail.com>
2024-01-06 18:34:57 +0200

    BookingForm view, submitting booking doesn't work

---
+++ src/views/customer/BookingSubmitted.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="booking-submitted">
+    <h1>Booking Submitted</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.booking-submitted {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+h1 {
+  text-align: center;
+  color: #333;
+}
+
+</style>
